415ee64e434d9bdd779f26035bf6f383f65320b8
kartik jindal <kartikjindal58@gmail.com>
2023-06-11 02:20:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+// import Greet from './components/Greet1'
 
 function App() {
+  const [newItem, setnewItem] = useState('');
+  const [todos, settodos] = useState([]);
+  function handlesubmit(e) {
+    e.preventDefault();
+    settodos([...todos, { id: crypto.randomUUID(), name: newItem, completed: false }]);
+    console.log(todos);
+    setnewItem("");
+  }
+  function toggle(id, completed) {
+    settodos(currenttodos => {
+      return (currenttodos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = completed;
+          return { ...todo, completed };
+        }
+        return todo;
+      }))
+    })
+  }
+  function deleteid(id){
+      settodos(currenttodos=>{
+        return currenttodos.filter(todo=> todo.id!=id)
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='new-item-form' onSubmit={handlesubmit}>
+        <div className='form-row'>
+          <label htmlFor='item'>Add new item in todo list</label>
+          <input type='text' value={newItem} onChange={(e) => { setnewItem(e.target.value) }} id='item' />
+        </div>
+        <button className='btn' > Add</button>
+      </form>
+      <h1>Todo list</h1>
+      <ul className='list'>
+        {todos.map((todo => {
+          return <li key={todo.id}>
+            <label>
+              <input checked={todo.completed}
+                onChange={
+                  (e) => { toggle(todo.id, e.target.checked) }}
+                type='checkbox' />
+              {todo.name}
+            </label>
+            <button className='btn btn-danger' 
+            onClick={
+              ()=>deleteid(todo.id)
+            }
+              >Delete</button>
+          </li>
+        }
+        ))
+
+        }
+      </ul>
     </div>
   );
 }
---
+++ src/components/Greet1.js
@@ -0,0 +1,9 @@
+import react from 'react'
+ 
+export default function Greet(){ 
+    return <div>
+<h1>hello world</h1>
+<p>hello</p>
+</div>
+}
+// export default Greet
